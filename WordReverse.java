ad66e5928608182462dce564aa6edcdf1b3db55c
prabu23568 <84140715+prabu23568@users.noreply.github.com>
2022-02-21 10:47:52 +0530

    Add files via upload
    String word reerse

---
+++ WordReverse.java
@@ -0,0 +1,24 @@
+package java_lerning;
+
+public class WordReverse {
+ public static void main(String[] args) {
+	
+	 String input="I am from trichy";
+		//output: I ma from ebc
+	 
+	 
+		String[] words = input.split(" ");
+		
+		for (int i = 0; i < words.length; i++) {
+			
+			if(i%2==1) {
+				for (int j = words[i].length()-1; j>=0; j--) {
+					System.out.print(words[i].charAt(j));
+				}
+				System.out.print(" ");
+				
+				}else {
+					System.out.print(words[i]+" ");
+}
+}
+}}
